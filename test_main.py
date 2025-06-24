fdad87f79730104e9e19325cf3d69c64fb3fe672
gREatNEss <rene.boat23@gmail.com>
2025-06-24 21:38:15 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,6 +6,6 @@ class Tester(unittest.TestCase):
     self.assertEqual(add(2,3),5)
     self.assertEqual(add(5,6),11)
 
-if ___name___ == '___main___':
+if __name__ == '__main__':
   unittest.main()
   
